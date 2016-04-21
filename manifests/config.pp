d9731619ea7d9f93dfb38a4439091a1a1ad3747b
Rudy YAYON <ryayon@outlook.com>
2016-04-21 10:37:43 +0200

    [CONFIG] mydestination variabilized

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,7 @@
 class postfix::config (
   $file_path       = $postfix::config_file_path,
   $file_name       = $postfix::config_file_name,
+  $mydestination   = $postfix::config_mydestination,
   $myhostname      = $postfix::config_myhostname,
   $myorigin        = $postfix::config_myorigin,
   $inet_interfaces = $postfix::config_inet_interfaces,
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@ class postfix (
   $package_ensure         = installed,
   $config_file_path       = '/etc/postfix',
   $config_file_name       = 'main.cf',
+  $config_mydestination   = [],
   $config_myhostname      = $::fqdn,
   $config_myorigin        = $::fqdn,
   $config_inet_interfaces = 'all',
